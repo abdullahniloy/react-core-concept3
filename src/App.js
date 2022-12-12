81d15aa4de0bc662d07d99a472ff2a2800a03f1c
niloyabdullah <abdullahniloy149@gmail.com>
2022-12-12 13:05:09 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>HI REACT</h1>
+      <Count></Count>
     </div>
   );
+}
+
+function Count() {
+  // const abc = useState(9);
+  const [count, setCount] = useState(1);
+  console.log(count)
+  const increaseCount = () => setCount(count + 1);
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+
+  const decreaseValue = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount} >Increase</button>
+      <br />
+      <button onClick={decreaseValue}> Decrease</button>
+    </div>
+  )
 }
 
 export default App;
